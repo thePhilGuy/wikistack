dd40c2ad4862c2c02d94e7d2e1aad5779c18faa5
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-03-02 22:47:07 -0500

    Serving new page form

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BROWSE MY WIKISTACK' });
+  res.render('index', { title: 'Browse my Wikistack' });
 });
 
 router.get('/about_us', function(req, res, next) {
 	res.render('about_us', {});
 });
+
+router.get('/new_page', function(req, res, next) {
+	res.render('new_page', { title: 'Add Page'});
+})
 
 module.exports = router;
